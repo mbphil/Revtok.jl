12c32c69468563f255310e0a32b29dc707a35fc8
mb <minter@posteo.de>
2019-03-16 11:53:15 +0100

    test update to julia 1.0

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,11 @@
 using Revtok
-using Base.Test
+using Test
+import Base.Unicode: normalize
 
 # write your own tests here
 @test begin
-    text = replace(normalize_string(readstring(
-        download("http://www.gutenberg.org/cache/epub/1661/pg1661.txt"))[4:end],
-        newline2lf=true), r"\n+", s->length(s) == 1 ? " " : "\n" ^ (length(s) - 1));
+    text = replace(normalize(read(
+        download("http://www.gutenberg.org/cache/epub/1661/pg1661.txt"), String)[4:end],
+        newline2lf=true), r"\n+" => s->length(s) == 1 ? " " : "\n" ^ (length(s) - 1));
     detokenize(tokenize(text)) == text
 end
